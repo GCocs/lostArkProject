e540d055ffb73bfeb8186af58a57980f7c10567c
intellij-Laptop <ysh062100@gmail.com>
2025-01-29 08:19:43 +0900

    fix[수집형포인트] : 수집형포인트 차트가 나오지 않는 오류 수정

--- src/main/java/com/teamProject/lostArkProject/collectible/controller/CollectibleRestController.java
+++ src/main/java/com/teamProject/lostArkProject/collectible/controller/CollectibleRestController.java
@@ -3,6 +3,7 @@ package com.teamProject.lostArkProject.collectible.controller;
 import com.teamProject.lostArkProject.collectible.domain.CharacterInfo;
 import com.teamProject.lostArkProject.collectible.dto.CollectiblePointSummaryDTO;
 import com.teamProject.lostArkProject.collectible.service.CollectibleService;
+import com.teamProject.lostArkProject.member.domain.Member;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ public class CollectibleRestController {
     // collectibles url로 GET 요청 시 json 데이터 반환
     @GetMapping("/collectibles")
     public List<CollectiblePointSummaryDTO> getCharacterCollectable(HttpSession session) {
-        String characterName = (String) session.getAttribute("nickname"); // http 세션에서 가져온 닉네임
-        return collectibleService.getCollectiblePointSummary(characterName);
+        Member member = (Member) session.getAttribute("member"); // http 세션에서 가져온 닉네임
+        return collectibleService.getCollectiblePointSummary(member.getMemberId());
     }
 }
